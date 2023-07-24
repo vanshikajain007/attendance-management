b22e5f138150b23b5d7bf6b0bf985b3c4178cb3d
vanshikajain007 <136046296+vanshikajain007@users.noreply.github.com>
2023-07-24 12:20:21 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+db_host = 'localhost'
+db_name = 'postgres'
+db_user = 'postgres'
+db_password = 'vanshika'
+db_port = '5432'
+
+get_db_connection = lambda: psycopg2.connect(
+    host=db_host,
+    dbname=db_name,
+    user=db_user,
+    password=db_password,
+    port=db_port
+)
